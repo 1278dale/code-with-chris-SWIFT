a0561267ae4e21568c72ab1870ef0b3416320299
Dale <dalestevens1278@gmail.com>
2015-03-12 15:55:15 +0000

    Playing around with classes in playground

---
+++ Classes.playground/section-1.swift
@@ -0,0 +1,17 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+class Person {
+    
+    init () {
+        println("New person initialised")
+        /**any code between curly braces gets executed when a new object of this class is created */
+    }
+}
+
+Person() /**created a new object */
+
+var a = Person() /** assigned the person object to the variable a, can reference the person class using 'a' */
