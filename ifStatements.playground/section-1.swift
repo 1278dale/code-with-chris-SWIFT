91ba98c3584ca3eaf56541702bf0e29aaddeb358
Dale <dalestevens1278@gmail.com>
2015-03-13 17:03:54 +0000

    Learning if statements in xCode playground

---
+++ ifStatements.playground/section-1.swift
@@ -0,0 +1,17 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var a = 13
+
+if a < 10 {
+    println("a is less than 10")
+}
+else if a > 10 && a < 15 {
+    println("a is greater than 10 but less than 15")
+}
+else {
+    println("A is more than 10")
+}
